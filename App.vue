1977f0afc7dde5e7f634c13ea7800bb5c212711f
RaresTecno <rarestecno@gmail.com>
2024-03-26 21:28:12 +0100

    header + nav

--- App.vue
+++ App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/header.vue'
+import Nav from './components/BarraLateral.vue'
+import './assets/index.css'
 
 </script>
-
 <template>
   <Header />
+  <Nav />
   <RouterView />
 </template>

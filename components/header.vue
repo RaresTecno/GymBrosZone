44d1488f695472a5cdc1c9fb5dc77958470cc53d
RaresTecno <rarestecno@gmail.com>
2024-03-26 18:44:10 +0100

    limpio

--- components/header.vue
+++ components/header.vue
@@ -1,12 +1,12 @@
+<script setup>
+import '../assets/header.css'
+
+</script>
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/" id="home">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>

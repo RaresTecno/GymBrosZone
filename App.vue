d1d0184cb6c9714bf464bdb8e62114ccdf23eed2
AltMarius <126814372+AltMarius@users.noreply.github.com>
2024-03-26 21:43:29 +0100

    Inicio (nada)

--- App.vue
+++ App.vue
@@ -1,10 +1,26 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/header.vue'
-
 </script>
 
 <template>
-  <Header />
+  <Header></Header>
   <RouterView />
 </template>
+
+<style>
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+:root{
+    --bg-color:#DBE4F6;
+    --dark-blue:#0B1E44;
+    --alt-black:#121212;
+    --greeny-cyan:#caeff6;
+}
+body{
+    background-color: var(--bg-color);
+}
+</style>

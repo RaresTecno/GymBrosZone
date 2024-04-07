213e9489abb8cd57c2b07852697f0adc6fc94dee
rauldomrob <126198547+rauldomrob@users.noreply.github.com>
2024-04-07 20:10:33 +0200

    login y register

--- App.vue
+++ App.vue
@@ -2,11 +2,13 @@
 import { RouterLink, RouterView } from 'vue-router'
 import Header from './components/header.vue'
 import Nav from './components/BarraLateral.vue'
+import Log from './views/Log.vue'
 import './assets/index.css'
 
 </script>
 <template>
-  <Header />
-  <Nav />
+  <!-- <Header /> -->
+  <!-- <Nav /> -->
+  <Log />
   <RouterView />
 </template>
---
+++ views/Log.vue
@@ -0,0 +1,21 @@
+
+
+
+<script setup>
+    import Register from '../components/Register.vue';
+    import Login from '../components/Login.vue';
+    import { ref } from 'vue';
+
+
+    const mostrarRegisterBool = ref(false);
+
+    const mostrarRegister = () => {
+        mostrarRegisterBool.value = true;
+    }
+
+
+</script>
+    <template>
+        <Register v-if="mostrarRegisterBool"/>
+        <Login v-else @irARegister="mostrarRegister"/>
+    </template>

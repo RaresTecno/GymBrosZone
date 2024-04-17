13b0199eece48a0787b0b292f8900bd642e8a53a
Rares Andrei Ana <rarestecno@gmail.com>
2024-04-17 09:52:29 +0200

    routes

--- router/index.js
+++ router/index.js
@@ -2,8 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/Home.vue'
 import ProfileView from '../views/Profile.vue'
 import Log from '../views/Log.vue'
-import Login from '../components/Login.vue';
-import Register from '../components/Register.vue';
 import Politicas_y_condiciones from '../views/Politicas_y_condiciones.vue'
 
 
@@ -21,18 +19,25 @@ const router = createRouter({
       component: ProfileView
     },
     {
-      path: '/log/', 
+      path: '/log', 
       component: Log,
       children: [
         {
-          path: 'login', 
-          component: Login
+          path: 'login',
+          name: 'login',
+          component: () => import('../components/Login.vue')
         },
         {
           path: 'register',
-          component: Register
+          name: 'register',
+          component: () => import('../components/Register.vue')
         }
       ]
+    },
+    // Ruta comodín para manejar rutas no encontradas
+    {
+      path: '/:catchAll(.*)',
+      redirect: '/'
     }
     // {
     //   path: '/politicas_y_condiciones',

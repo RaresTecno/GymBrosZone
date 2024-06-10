ea720119b7ba12e69f386f1ba8237a00a957a901
Rares Andrei Ana <rarestecno@gmail.com>
2024-06-10 16:13:17 +0200

    Delete EditProfile.vue

--- components/EditProfile.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-    import { ref, onMounted } from "vue";
-    import { supabase } from "@/clients/supabase";
-</script>
-
-<template>Editando Perfil</template>
-
-<style scoped></style>
